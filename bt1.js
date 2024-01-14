573347c7df661819e507a9f19ca27126575227fc
duyanh020405 <147359622+duyanh020405@users.noreply.github.com>
2024-01-15 01:52:19 +0700

    Add files via upload

---
+++ bt1.js
@@ -0,0 +1,4 @@
+var n =parseInt(prompt("nhap so n"));
+for (let i=0 ; i<n ; i++){
+    console.log(i);
+}
---
+++ bt2.js
@@ -0,0 +1,6 @@
+var n =parseInt(prompt("nhap so n"));
+var gt = 1 ;
+for ( let i = 1 ; i<n+1 ; i++){
+    gt *= i ;
+}
+console.log(gt);
